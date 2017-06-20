4724ffe770a76945326f07013055ab3b592f761d
Leszek Marcjan <leszek.marcjan@algolytics.pl>
2017-06-20 18:53:13 +0200

    dodanie uzytkownika admin

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -3,6 +3,10 @@ USER=${MONGODB_USERNAME:-mongo}
 echo $USER > /.mongo_user
 PASS=${MONGODB_PASSWORD:-$(pwgen -s -1 16)}
 echo $PASS > /.mongo_pass
+ADMIN_USER='userAdmin'
+echo $ADMIN_USER > /.mongo_admin_user
+ADMIN_PASS=`pwgen -s -1 16`
+echo $ADMIN_PASS > /.mongo_admin_pass
 DB=${MONGODB_DBNAME:-admin}
 echo $DB > /.mongo_db
 if [ ! -z "$MONGODB_DBNAME" ]
@@ -18,16 +22,27 @@ echo "FIRSTRUN MongoDB User: \"$USER\""
 echo "FIRSTRUN MongoDB Password: \"$PASS\""
 echo "FIRSTRUN MongoDB Database: \"$DB\""
 echo "FIRSTRUN MongoDB Role: \"$ROLE\""
+echo "FIRSTRUN MongoDB Admin User: \"$ADMIN_USER\""
+echo "FIRSTRUN MongoDB Admin Password: \"$ADMIN_PASS\""
 echo "========================================================================"
 
 # Start MongoDB service
 echo "FIRSTRUN Starting MongoDB..."
+if [ -f /data/mongod.lock ]; then
+/usr/bin/mongod --dbpath /data &
+else
 /usr/bin/mongod --dbpath /data --nojournal &
-while ! nc -vz localhost 27017; do echo "FIRSTRUN sleep 1"; sleep 1; done
+fi
+
+while ! nc -vz localhost 27017; do echo "FIRSTRUN waiting for MongoDB"; sleep 1; done
 
 # Create User
 echo "Creating user: \"$USER\"..."
 mongo $DB --eval "db.createUser({ user: '$USER', pwd: '$PASS', roles: [ { role: '$ROLE', db: '$DB' } ] });"
+
+# Create Admin
+echo "Creating admin..."
+mongo $DB --eval "db.createUser({ user: '$ADMIN_USER', pwd: '$ADMIN_PASS', roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ] });"
 
 # Stop MongoDB service
 echo "FIRSTRUN Stoping MongoDB..."
--- scripts/init_run.sh
+++ scripts/init_run.sh
@@ -15,15 +15,14 @@ echo "========================================================================"
 # Start MongoDB service
 echo "INITRUN Starting MongoDB..."
 /usr/bin/mongod --dbpath /data &
-while ! nc -vz localhost 27017; do echo "INITRUN sleep 1"; sleep 1; done
+
+while ! nc -vz localhost 27017; do echo "INITRUN waiting for MongoDB"; sleep 1; done
 
 echo "INITRUN Running initialization scripts..."
 if [[ -e /scripts/init_script.js ]]; then
   mongo $DB /scripts/init_script.js
   rm -f /scripts/init_script.js
 fi
-
-sleep 30
 
 # Stop MongoDB service
 echo "INITRUN Stoping MongoDB..."
